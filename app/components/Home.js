e32ec8b8276be55b8ed5cd4e17dbf73c6a058cf4
mattmcguin <matt.mcguiness@portalanalytics.us>
2016-06-20 23:51:29 -0400

    Revert "Material UI Import" (#9)

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,18 +1,7 @@
-'use strict';
-
 var React = require('react');
 var styles = require('../styles');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
-let beerImageSrc = require('../../Two-Glasses-of-Beer-6.png');
-
-// Material
-import {RaisedButton, Dialog, FlatButton, AppBar} from 'material-ui';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-
-const muiTheme = getMuiTheme();
 
 var Home = React.createClass({
 
@@ -36,12 +25,7 @@ var Home = React.createClass({
         this.setToLocalStorage('drinkList');
         this.getFromLocalStorage('drinkList');
         return (
-        <MuiThemeProvider muiTheme={muiTheme}>
             <div>
-              <AppBar
-                title="Title"
-                iconClassNameRight="muidocs-icon-navigation-expand-more"
-              />
               <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
                 <h1> Mixology University </h1>
                 <Link to='/menu'>
@@ -49,7 +33,7 @@ var Home = React.createClass({
                 </Link>
                 <Link to='/homebrew'>
                 <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
-                <img src={beerImageSrc} />
+                <img src='../../Two-Glasses-of-Beer-6.png' />
                 </div>
                 </Link>
                 <Link to='/barpage'>
@@ -57,8 +41,6 @@ var Home = React.createClass({
                 </Link>
               </div>
             </div>
-          </MuiThemeProvider>
-
         );
       }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,22 @@
-const webpack = require('webpack');
-const path = require('path');
-const buildPath = path.resolve(__dirname, 'build');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
-const TransferWebpackPlugin = require('transfer-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
 
-const config = {
-  // Entry points to the project
+module.exports = {
   entry: [
-    'webpack/hot/dev-server',
-    'webpack/hot/only-dev-server',
-    path.join(__dirname, '/app/index.js'),
+    './app/index.js',
   ],
-  // Server Configuration options
-  devServer: {
-    contentBase: 'dist', // Relative directory for base of server
-    devtool: 'eval',
-    hot: true, // Live-reload
-    inline: true,
-    port: 3000, // Port Number
-    host: 'localhost', // Change to '0.0.0.0' for external facing server
-  },
-  devtool: 'eval',
   output: {
-    path: buildPath, // Path of output file
-    filename: 'index.js',
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js',
   },
-  plugins: [
-    // Enables Hot Modules Replacement
-    new webpack.HotModuleReplacementPlugin(),
-    // Allows error warnings but does not stop compiling.
-    new webpack.NoErrorsPlugin(),
-    // Moves files
-    new TransferWebpackPlugin([
-      {from: 'dist'},
-    ], path.resolve(__dirname)),
-  ],
   module: {
     loaders: [
-        // React-hot loader and
-         // All .js files
-        { test: /\.js$/, loader: 'react-hot', exclude: [nodeModulesPath]},
-        { test: /\.js$/, loader: 'babel-loader', exclude: [nodeModulesPath]},
-        { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192', exclude: [nodeModulesPath] }, 
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', },
     ],
   },
+  plugins: [HtmlWebpackPluginConfig],
 };
-
-module.exports = config;
